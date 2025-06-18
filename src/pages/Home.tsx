c5ae79b4e0552a9d39fb4fd4a337e834f9f9815b
Utkal Medpro <ranjitmajhi00143@gmail.com>
2025-06-18 23:26:24 +0530

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,3 @@
-import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { Syringe, TestTube, Ban as Bandage, Clock, Shield, Users, Star, CheckCircle, Phone, Calendar } from 'lucide-react'
 import DisclaimerModal from '../components/DisclaimerModal'
